39a0d1ef7af622e02ebe58682fc640d7f819a9a4
Emmanuel <rivera128@live.com>
2020-12-03 13:05:45 -0600

    added gets and passing to API

--- lib/ffxiv/api.rb
+++ lib/ffxiv/api.rb
@@ -2,11 +2,8 @@ class API
     attr_accessor :fc_name
     BASE_URL = "https://xivapi.com"
 
-    def initialize(fc_name)
-        @fc_name = fc_name
-    end
-    
     def call(fc_name)
+        @fc_name = fc_name
         url = BASE_URL + "/freecompany/search?name=#{@fc_name}"
         uri = URI(url)
         binding.pry
--- lib/ffxiv/cli.rb
+++ lib/ffxiv/cli.rb
@@ -1,3 +1,18 @@
 class CLI
+    def initialize
+        greeting
+        provide_fc_name
+    end
 
+    def greeting
+        puts "Welcome to the FFXIV Free Company Search"
+    end
+
+    def provide_fc_name
+        puts "Please provide name for Free Company"
+        free_company = gets
+        puts free_company = free_company.chomp
+        # puts free_company
+        API.call(free_company) 
+    end
 end
