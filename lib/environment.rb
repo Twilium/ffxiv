31ecb8e45b45e259779e36a72690fa57e9ec476f
Emmanuel <rivera128@live.com>
2020-12-03 12:13:36 -0600

    modified environment.rb and added ffxiv executable

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,10 @@
-require "ffxiv/version"
+require_relative "./ffxiv/version"
+require_relative "./ffxiv/cli"
+require_relative "./ffxiv/api"
+require_relative "./ffxiv/free_company"
+require_relative "./ffxiv/free_company_search"
 
-
+require "pry"
 require "httparty"
 require "net/http"
 module Ffxiv
