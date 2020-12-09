511735899fe2296000b4b82566a48c1ff1341358
Emmanuel <rivera128@live.com>
2020-12-09 11:46:45 -0600

    added colors to output messages and cleaned up iteration

--- lib/ffxiv/cli.rb
+++ lib/ffxiv/cli.rb
@@ -10,14 +10,14 @@ class CLI
         # puts " "
 
         puts "┌───────────────────────────────────────────┐"
-        puts "│ " + "Welcome to the FFXIV Free Company Search!".blink.cyan + " │"
+        puts "│ " + "Welcome to the FFXIV Free Company Search!".blink.light_cyan + " │"
         puts "└──────────────∩─────────∩──────────────────┘"
-        puts "...............l(` ･ω ･´) l                  "
+        puts "..............." + "l(` ･ω ･´) l                  ".magenta
     end
 
     def choices
         prompt = TTY::Prompt.new
-        if prompt.yes?("Would you like to add a server?".light_cyan)
+        if prompt.yes?("Would you like to add a server?".cyan)
             provide_fc_name_with_server
         else
             pull_free_company_no_server
@@ -25,7 +25,7 @@ class CLI
     end
 
     def provide_fc_name
-        puts "Please provide name for Free Company".cyan.bold
+        puts "Please provide name for Free Company".cyan
         @free_company = gets.strip
         puts " "
         choices
@@ -41,7 +41,7 @@ class CLI
 
     def provide_fc_name_with_server
         puts ""
-        puts "Please provide server name".cyan.bold
+        puts "Please provide server name".cyan
         @server_name = gets.strip
         puts " "
         @free_companies = API.get_free_company_with_server_name(@free_company, @server_name)
@@ -50,7 +50,7 @@ class CLI
 
     def display_free_companies
         if @free_companies.length > 35
-            puts "Too many results. Please try a more specific name or add a server name.".bold.cyan
+            puts "Too many results. Please try a more specific name or add a server name.".cyan
             puts " "
             provide_fc_name
         elsif @free_companies.length == 0
@@ -72,8 +72,8 @@ class CLI
 
     def company_member_selection_or_new_search
         prompt = TTY::Prompt.new
-        if prompt.yes?("Would you like to see members of a Free Company?")
-            puts "Please type number of desired Free Company:"
+        if prompt.yes?("Would you like to see members of a Free Company?".cyan)
+            puts "Please type number of desired Free Company:".cyan
             input = gets.strip.downcase
             puts " "
             if input.to_i.between?(1, @free_companies.length)
@@ -81,7 +81,7 @@ class CLI
                 @fc_members = API.get_free_company_member_by_id(company.id)
                 display_free_company_members
             else
-                puts "This number is not in range. Please try again."
+                puts "This number is not in range. Please try again.".cyan
                 puts " "
                 company_member_selection_or_new_search
             end
@@ -101,18 +101,18 @@ class CLI
             print member_table.render(:unicode)
         end
             puts ""
-            puts "<- all ->" if @i != @fc_members.length
-		    puts "<- less  " if @i == @fc_members.length
-		    puts "  next ->" if @i == 0
-		    puts "<- previous |" + "| next ->" if @i >= 50 && @i+49 <@fc_members.length
-		    puts "<- previous  " if @i != 0
+            puts "<- all ->".red if @i != @fc_members.length
+		    puts "<- less  ".yellow if @i == @fc_members.length
+		    puts "  next ->".green if @i == 0
+            puts "  next ->".green if @i >= 50 && @i+49 <@fc_members.length
+            puts "<- previous  ".blue if @i != 0 && @i != @fc_members.length
 		    puts ""
-		    puts "type ALL to see the full list."
-		    puts "type LESS from the full list to return to the truncated list."
-		    puts "type NEXT to page through the list 50 at a time."
-		    puts "type PREVIOUS to return to the preview view."
-            puts "type SEARCH to do another search query."
-            puts "type EXIT to close the program."
+		    puts "type " + "ALL".red + " to see all members."
+		    puts "type " + "LESS".yellow + " from the full list to return to the truncated list."
+		    puts "type " + "NEXT".green + " to page through the list 50 members at a time."
+		    puts "type " + "PREVIOUS".blue + " to return to the preview page."
+            puts "type " + "SEARCH".light_magenta + " to do another search query."
+            puts "type " + "EXIT".magenta + " to close the program."
             puts " "
             page_iteration
     end
@@ -126,36 +126,37 @@ class CLI
             @i = 0
             display_free_company_members
         elsif input == "next" && @i+50 > @fc_members.length
-            puts "Sorry, that's the end of the list!"
-            puts "Please try a different command: "
+            puts "Sorry, that's the end of the list!".red
+            puts "Please try a different command: ".cyan
             page_iteration
         elsif input == "next"
             @i += 50
             display_free_company_members
         elsif input == "previous" && @i == 0
-            puts "This is the beginning of the list!"
-            puts "Please try again: "
+            puts "This is the beginning of the list!".red
+            puts "Please try again: ".cyan
             page_iteration
         elsif input == "previous" && @i == @fc_members.length
-            puts "This is the beginning of the list!"
-            puts "Please try again: "
+            puts "This is the entire list!".red
+            puts "Please try again: ".cyan
             page_iteration
         elsif input == "previous"
             @i -= 50
+            display_free_company_members
         elsif input == "search"
             search_again_or_exit
         elsif input == "exit"
             goodbye
         else 
-            puts "Sorry, that is not a valid command!"
-            puts "Please try again: "
+            puts "Sorry, that is not a valid command!".cyan
+            puts "Please try again: ".cyan
             page_iteration
         end
     end
 
     def search_again_or_exit
         prompt = TTY::Prompt.new
-        if prompt.yes?("Would you like to do another search?")
+        if prompt.yes?("Would you like to do another search?".cyan)
             provide_fc_name
         else
             goodbye
